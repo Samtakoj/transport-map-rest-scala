ccdef22e12858fdf405aa3d83893a77023c9c575
Artsiom <artkostm@gmail.com>
2017-02-20 19:32:40 +0300

    transport api for stops is done

--- src/main/scala/com/transport/map/rest/schedule/TransportApi.scala
+++ src/main/scala/com/transport/map/rest/schedule/TransportApi.scala
@@ -1,28 +1,54 @@
 package com.transport.map.rest.schedule
 
-import akka.actor.ActorSystem
-import akka.event.LoggingAdapter
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-import akka.stream.Materializer
-import akka.stream.scaladsl.{Flow, Sink, Source}
-import com.typesafe.config.Config
+import java.io.IOException
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.transport.map.rest.AkkaAware
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
-  * Created by arttsiom.chuiko on 19/02/2017.
+  * Created by artsiom.chuiko on 19/02/2017.
   */
-trait TransportApi {
-  implicit val system: ActorSystem
-  implicit def executor: ExecutionContextExecutor
-  implicit val materializer: Materializer
-
-  def config: Config
-  val logger: LoggingAdapter
+trait TransportApi extends AkkaAware{
 
   lazy val transApiConnectionFlow: Flow[HttpRequest, HttpResponse, Any] =
     Http().outgoingConnection(config.getString("services.schedule.host"), config.getInt("services.schedule.port"))
 
   def transApiRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request).via(transApiConnectionFlow).runWith(Sink.head)
+
+  def fetchData(path: String): Future[Either[String, String]] = {
+    transApiRequest(RequestBuilding.Get(path)).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[String].map(Right(_))
+        case BadRequest => Future.successful(Left(s"${response.status} status was received"))
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          val error = s"Transport Api request failed with status code ${response.status} and entity $entity"
+          logger.error(error)
+          Future.failed(new IOException(error))
+        }
+      }
+    }
+  }
+
+  def fetchStops() = complete {
+    fetchData(config.getString("services.schedule.stops")).map[ToResponseMarshallable] {
+      case Right(data) => data
+      case Left(errorMessage) => BadRequest -> errorMessage
+    }
+  }
+
+  def createStopsRoute()(implicit executor: ExecutionContext): Route = get {
+    path("stops") {
+      fetchStops()
+    }
+  }
 }
